212ed95bd63d1a4e1f15054b55a9a1c250b4503a
Kuzm1ch88 <steam.danya@yandex.ru>
2022-09-16 15:03:18 +0300

    Fabric Files

--- src/main/java/ru/kuzm1ch88/ethernalmod/EthernalMod.java
+++ src/main/java/ru/kuzm1ch88/ethernalmod/EthernalMod.java
@@ -5,10 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EthernalMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "ethernalmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +14,6 @@ public class EthernalMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Ethernal Mod!");
 	}
 }
